6200b998b8ca4714dc8d0927ea6db664439e4e37
williamslsy <waleebaftp@gmail.com>
2022-07-24 15:48:40 +0100

    first commit

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,12 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+      About
+    </>
+  )
+}
+
+export default AboutPage
